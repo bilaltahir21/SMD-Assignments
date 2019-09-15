17cbe80e6c8ffedeb95fce466d5ce0676cf85e33
billztee7 <bilaltahir3636@gmail.com>
2019-09-16 03:06:10 +0500

    Extra files cleaned

--- Assignment_1-Q5/src/Communication.java
+++
@@ -1,9 +0,0 @@
-public interface Communication {
-    String continuePlaying();
-
-    String exitGame();
-
-    void end();
-
-    boolean isRunning();
-}
--- Assignment_1-Q5/src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        ProcessA process=new ProcessA();
-        Application app=new Application(process);
-    }
-}
